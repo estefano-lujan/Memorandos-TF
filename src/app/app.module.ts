89f2b7d2b7e2ba04ca6fd387d343860901f3d86d
Estefano-lujan <lujan.estefano@hotmail.com>
2021-09-03 20:08:21 -0300

    Intentando crear rutas entre componentes mediante router outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormularioComponent } from
 './components/formulario/formulario.component';
-import { ReactiveFormsModule} from '@angular/forms';
-import { EnviarMemorandosComponent } from './components/enviar-memorandos/enviar-memorandos.component';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { EnviarmemorandosComponent, } from './components/enviar-memorandos/enviar-memorandos.component';
 import { RecibiendoMemorandosComponent } from './components/recibiendo-memorandos/recibiendo-memorandos.component';
+
 
 @NgModule({
 declarations: [
 AppComponent,
 FormularioComponent,
-EnviarMemorandosComponent,
+EnviarmemorandosComponent,
 RecibiendoMemorandosComponent
 ],
 imports: [
 BrowserModule,
 AppRoutingModule,
 ReactiveFormsModule,
+FormsModule
 ],
 providers: [],
 bootstrap: [AppComponent]
